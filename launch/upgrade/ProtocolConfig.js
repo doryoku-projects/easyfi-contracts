24858133263cd17005f0a1d6aa55abd7ab6f45dd
0xkaranchauhan <kcbizzkc@gmail.com>
2025-09-12 20:49:08 +0530

    minor changes

--- launch/upgrade/ProtocolConfig.js
+++ launch/upgrade/ProtocolConfig.js
@@ -3,25 +3,28 @@ require("dotenv").config();
 
 async function main() {
   const proxyAddress = process.env.PROTOCOL_CONFIG_ADDRESS; // Reemplaza esto con la dirección del proxy ya desplegado
+  const implementationAddress = await upgrades.erc1967.getImplementationAddress(proxyAddress);
+  const protocolConfigContract = await ethers.getContractFactory("ProtocolConfigUpgradeable");
 
-  const protocolConfigContract = await ethers.getContractFactory(
-    "ProtocolConfigUpgradeable"
-  );
+  console.log("Current implementation address:", implementationAddress);
+  console.log("ProtocolConfigUpgradeable")
+  console.log("[UPGRADE] Updating the proxy contract...");
 
-  console.log("Haciendo upgrade del contrato...");
-
+  // The proxy contract is updated
   const upgraded = await upgrades.upgradeProxy(
     proxyAddress,
     protocolConfigContract
   );
 
-  console.log(
-    "Contrato actualizado correctamente, nueva versión desplegada en:",
-    await upgraded.getAddress()
-  );
+  console.log("[UPGRADE] Proxy contract updated at:", await upgraded.getAddress());
+  const NewimplementationAddress = await upgrades.erc1967.getImplementationAddress(proxyAddress);
+
+  console.log("Current implementation address:", NewimplementationAddress);
 }
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error("[UPGRADE] Error in upgrade:", error);
+    process.exit(1);
+  });
--- launch/upgrade/Vault.js
+++ launch/upgrade/Vault.js
@@ -1,16 +1,13 @@
-// scripts/upgradeAggregator.js
 const { ethers, upgrades } = require("hardhat");
 require("dotenv").config();
 
 async function main() {
   const proxyAddress = process.env.VAULT_MANAGER_ADDRESS;  // Replace with the actual address
-
   const implementationAddress = await upgrades.erc1967.getImplementationAddress(proxyAddress);
+  const VaultManagerUpgradeable = await ethers.getContractFactory("VaultManagerUpgradeable");
 
   console.log("Current implementation address:", implementationAddress);
-  const VaultManagerUpgradeable =
-    await ethers.getContractFactory("VaultManagerUpgradeable");
-console.log("vault manager upgrable")
+  console.log("vault manager upgrable")
   console.log("[UPGRADE] Updating the proxy contract...");
 
   // The proxy contract is updated
@@ -18,10 +15,9 @@ console.log("vault manager upgrable")
     proxyAddress,
     VaultManagerUpgradeable
   );
-  console.log("[UPGRADE] Proxy contract updated at:",
-    await upgradedVault.getAddress()
-  );
-    const NewimplementationAddress = await upgrades.erc1967.getImplementationAddress(proxyAddress);
+
+  console.log("[UPGRADE] Proxy contract updated at:", await upgradedVault.getAddress());
+  const NewimplementationAddress = await upgrades.erc1967.getImplementationAddress(proxyAddress);
 
   console.log("Current implementation address:", NewimplementationAddress);
 }
