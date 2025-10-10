19e92ed9459ca411089682bb2803b9a5405eafd5
yuvi-deva-31 <yuvaraj@doryoku.tech>
2025-10-10 11:52:37 +0530

    fix: name changes

--- contracts/UserAccessControl.sol
+++ contracts/UserAccessControl.sol
@@ -129,10 +129,10 @@ abstract contract UserAccessControl is UUPSUpgradeable, AccessControlErrors {
         _;
     }
     /**
-     * @notice notEmergency modifier
+     * @notice onEmergency modifier
      * @dev This modifier checks if the contract is in emergency mode.
      */
-    modifier isEmergency() {
+    modifier onEmergency() {
         if (!s_userManager.isEmergency()) revert UAC_NOT_IN_EMERGENCY_MODE();
         _;
     }
