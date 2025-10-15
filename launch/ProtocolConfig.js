8817a542b859c919cd4eb2c241fc80beb2650bfc
SDargarh-doryoku <shubhangi@doryoku.tech>
2025-10-15 16:06:47 +0530

    added uniswap and token address in config for chains

--- launch/ProtocolConfig.js
+++ launch/ProtocolConfig.js
@@ -3,7 +3,7 @@ const { getDeploymentAddress } = require("./DeploymentStore");
 const CONFIG = require("./config");
 
 async function deployProtocolConfig() {
-  const { WALLETS, EXTERNAL, PROTOCOL } = CONFIG;
+  const { WALLETS, EXTERNAL, PROTOCOL, ADDRESSES_PER_CHAIN } = CONFIG;
 
   const USER_MANAGER = await getDeploymentAddress("UserManagerUpgradeable");
 
@@ -20,16 +20,19 @@ async function deployProtocolConfig() {
     "ClientAddress"
   ].map(createConfigKey);
 
+  const network = await ethers.provider.getNetwork();
+  const chainId = Number(network.chainId);
+
   const addressValues = [
-    WALLETS.MASTER_ADMIN,
-    WALLETS.MASTER_ADMIN,
-    WALLETS.MASTER_ADMIN,
-    WALLETS.MASTER_ADMIN,
-    WALLETS.MASTER_ADMIN,
-    EXTERNAL.UNISWAP_NFT,
-    EXTERNAL.SWAP_ROUTER,
-    EXTERNAL.FACTORY,
-    EXTERNAL.MAIN_TOKEN,
+    WALLETS.MASTER_ADMIN,     // VAULT_MANAGER 
+    WALLETS.MASTER_ADMIN,     // LIQ_MANAGER 
+    WALLETS.MASTER_ADMIN,     // LIQ_HELPER
+    WALLETS.MASTER_ADMIN,     // ORACLE_SWAP
+    WALLETS.MASTER_ADMIN,     // AGGREGATOR
+    ADDRESSES_PER_CHAIN[chainId].UNISWAP_NFT_ADDRESS,
+    ADDRESSES_PER_CHAIN[chainId].SWAP_ROUTER_ADDRESS,
+    ADDRESSES_PER_CHAIN[chainId].FACTORY_ADDRESS,
+    ADDRESSES_PER_CHAIN[chainId].MAIN_TOKEN_ADDRESS,
     EXTERNAL.CLIENT
   ];
 
