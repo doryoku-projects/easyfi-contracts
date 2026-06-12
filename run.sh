f01d2c1e0ec17ff238ae1e31313eabc6f772e20f
0xkaranchauhan <kcbizzkc@gmail.com>
2026-06-12 13:29:07 +0530

    chore: update dependencies and modify deployment scripts
    - Added new development dependencies: hardhat-network-helpers, typechain, and others for improved testing and development.
    - Updated deployment script to change WHITELABEL and network parameters for deployment and testing.

--- run.sh
+++ run.sh
@@ -6,9 +6,9 @@ set -e  # exit on error
 # npx hardhat run ./launch/factory.js --network localhost
 
 echo "🚀 Step 2: Deploy with contracts..."
-npx cross-env WHITELABEL="wadz" hardhat run ./launch/deployAll.js --network base                 
+npx cross-env WHITELABEL="netfi_test" hardhat run ./launch/deployAll.js --network arbitrumOne                 
 
 echo "🧪 Step 3: Run role setting updates..."
-npx cross-env WHITELABEL="wadz" hardhat test test/Integration/I_rolesetingNuevo.js --network base
+npx cross-env WHITELABEL="netfi_test" hardhat test test/Integration/I_rolesetingNuevo.js --network arbitrumOne
 
 echo "✅ Done! Contracts Deployed and tested successfully."
